cfbed72ea51f06056acaf3303065bebbdbb9ea1c
juhokauppala <juho.kauppala@gmail.com>
2025-11-19 23:54:59 +0200

    setup eleventy for version B

---
+++ B/eleventy.config.js
@@ -0,0 +1,11 @@
+import pugPlugin from "@11ty/eleventy-plugin-pug";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default function (eleventyConfig) {
+    eleventyConfig.addPlugin(pugPlugin)
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+    eleventyConfig.addWatchTarget("_includes/*.pug");
+    eleventyConfig.addPassthroughCopy("Merriweather-VariableFont_opsz,wdth,wght.ttf")
+    eleventyConfig.addPassthroughCopy("OugkehRegular-DYYrW.otf")
+    eleventyConfig.addPassthroughCopy("j.webp")
+};
---
+++ B/serve.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+npx @11ty/eleventy --serve
