11d50403194aa16ba6c465699edc4cf6dff1fe03
juhokauppala <juho.kauppala@gmail.com>
2026-08-04 10:59:44 +0000

    add encrypt local helper command

--- .devcontainer/postcreateCommand.sh
+++ .devcontainer/postcreateCommand.sh
@@ -3,3 +3,5 @@
 git config --global user.name juhokauppala \
     && git config --global user.email juho.kauppala@gmail.com
 npm install -g pagecrypt http-server
+
+echo 'alias encrypt="pagecrypt _participant_raw.html participant.html asd"' >> /home/vscode/.bashrc
