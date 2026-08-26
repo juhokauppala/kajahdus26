0df71d136982e166a18920a87f710fc45492fc02
juhokauppala <juho.kauppala@gmail.com>
2026-08-26 08:26:04 +0000

    move http-server to package deps

--- .devcontainer/postattachCommand.sh
+++ .devcontainer/postattachCommand.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-setsid http-server -p 8080 -c-1 > /tmp/http-server.log 2>&1 < /dev/null &
+setsid npm run serve > /tmp/http-server.log 2>&1 < /dev/null &
 echo "Starting http-server in the background. Logs are available at /tmp/http-server.log"
 while ! curl -s http://localhost:8080 > /dev/null; do
   sleep 0.5
