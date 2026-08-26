b13bdcb8632591cb3816b4aac820246ddcd266a3
juhokauppala <juho.kauppala@gmail.com>
2026-08-26 12:17:05 +0000

    enable fixed vite plugin

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,21 +1,7 @@
 import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
-import { encryptHTML } from "pagecrypt";
-
-const resolvePassword = () => {
-    if (process.env.PASSWORD) return process.env.PASSWORD;
-
-    console.warn(`No environment variable called PASSWORD!`)
-    return "asd"
-}
 
 export default async function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("images")
-    // eleventyConfig.addPlugin(EleventyVitePlugin);
-    eleventyConfig.addTransform("encrypt-participant", async function (content) {
-        const isSensitive = this.page.outputPath?.includes("participant")
-        if (isSensitive) return encryptHTML(content, resolvePassword());
-
-        return content
-    })
+    eleventyConfig.addPlugin(EleventyVitePlugin);
 };
